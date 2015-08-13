07638ba13d7b9545926ff3b289ba647dbdfa5fe7
syymza <syymza@gmail.com>
2015-08-13 10:53:57 +0100

    Added authentication to templates API

--- app/routes/templates.server.routes.js
+++ app/routes/templates.server.routes.js
@@ -1,18 +1,19 @@
 'use strict';
 
 const templates = require('../controllers/templates.server.controller');
+const ensureAuthenticated = require('../utils/ensureAuthenticated.server.utils');
 
 module.exports = (app) => {
 
     app.route('/templates')
-        .get(templates.list)
-        .post(templates.create);
+        .get(ensureAuthenticated, templates.list)
+        .post(ensureAuthenticated, templates.create);
 
     app.route('/templates/:templateId')
-        .get(templates.read)
-        .patch(templates.patch)
-        .put(templates.update)
-        .delete(templates.delete);
+        .get(ensureAuthenticated, templates.read)
+        .patch(ensureAuthenticated, templates.patch)
+        .put(ensureAuthenticated, templates.update)
+        .delete(ensureAuthenticated, templates.delete);
 
     app.param('templateId', templates.templateById);
 
