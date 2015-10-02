67988f030bf8cc2dbfe2f42d4af9d9f78e6722ba
Daniele Zanni <syymza@gmail.com>
2015-10-02 12:24:40 +0100

    Added cat image

--- app/controllers/uploadImage.server.controller.js
+++ app/controllers/uploadImage.server.controller.js
@@ -3,8 +3,9 @@
 module.exports = (req, res) => {
 
     return res.status(201).json({
-        url: 'http://www.example.com/image.jpg',
-        funcNum: req.query.CKEditorFuncNum
+        url: 'http://4.bp.blogspot.com/-XkviAtJ1s6Q/T3YFb2RUhDI/AAAAAAAAAVQ/EHomLZlFMKo/s1600/small+cat.jpg',
+        funcNum: req.query.CKEditorFuncNum,
+        someOtherProperty: 'someText'
     });
 
 };
