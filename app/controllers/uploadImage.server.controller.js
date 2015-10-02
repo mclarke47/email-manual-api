3333dddd286c7af97eacc631f66eae04b90aa51f
syymza <syymza@gmail.com>
2015-10-02 13:41:43 +0100

    Return req.files

--- app/controllers/uploadImage.server.controller.js
+++ app/controllers/uploadImage.server.controller.js
@@ -4,8 +4,7 @@ module.exports = (req, res) => {
 
     return res.status(201).json({
         url: 'http://4.bp.blogspot.com/-XkviAtJ1s6Q/T3YFb2RUhDI/AAAAAAAAAVQ/EHomLZlFMKo/s1600/small+cat.jpg',
-        funcNum: req.query.CKEditorFuncNum,
-        someOtherProperty: 'someText'
+        files: req.files
     });
 
 };
