896623d3b8bad335f9c9e6215d89df6e7e6198c1
syymza <syymza@gmail.com>
2015-10-08 09:51:24 +0100

    Updated documentation

--- app/routes/sendTestEmail.server.routes.js
+++ app/routes/sendTestEmail.server.routes.js
@@ -5,6 +5,27 @@ const sendTestEmail = require('../controllers/sendTestEmail.server.controller.js
 
 module.exports = (app) => {
     app.route('/send-test-email')
+        /**
+         * @api {post} /send-test-email Send a test email.
+         * @apiVersion 0.2.0
+         * @apiName SendTestEmail
+         * @apiGroup Send
+         *
+         * @apiUse AuthHeader
+         *
+         * @apiParam {ObjectId} email The ID of the email to be sent.
+         * @apiParam {String[]} recipients The array of FT recipients for the email.
+         *
+         * @apiSuccess {Number} messages_delivered Number of messages delivered.
+         *
+         * @apiSuccessExample Success-Response:
+         *     HTTP/1.1 201 OK
+         *
+         * {
+             "messages_delivered": 1
+         * }
+         *
+         */
         .post(ensureAuthenticated, sendTestEmail);
 
 
