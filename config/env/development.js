f4caeb38c8324e7371f8e4b99dd61fdf90c42b74
syymza <syymza@gmail.com>
2015-11-10 12:27:45 +0000

    Use MongoHQ instead of MongoLab

--- config/env/development.js
+++ config/env/development.js
@@ -3,7 +3,7 @@
 const port = process.env.PORT || 1337;
 const logLevel = process.env.LOG_LEVEL || 'info';
 const processId = process.env.DYNO || process.pid;
-const db = process.env.MONGOLAB_URI || 'mongodb://localhost/ft-email-manual-api-dev';
+const db = process.env.MONGOHQ_URL || 'mongodb://localhost/ft-email-manual-api-dev';
 const googleSecret = process.env.GOOGLE_SECRET;
 const tokenSecret = process.env.TOKEN_SECRET || 'devSecret';
 const authUser = process.env.BASIC_AUTH_USER || 'development';
--- config/env/test.js
+++ config/env/test.js
@@ -3,7 +3,7 @@
 const port = process.env.PORT || 1338;
 const logLevel = process.env.LOG_LEVEL || 'warn';
 const processId = process.env.DYNO || process.pid;
-const db = process.env.MONGOLAB_URI || 'mongodb://localhost/ft-email-manual-api-test';
+const db = process.env.MONGOHQ_URL || 'mongodb://localhost/ft-email-manual-api-test';
 const googleSecret = process.env.GOOGLE_SECRET || 'testSecret';
 const tokenSecret = process.env.TOKEN_SECRET || 'testSecret';
 const authUser = process.env.BASIC_AUTH_USER || 'test';
