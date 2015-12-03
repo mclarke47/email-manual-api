7cb64a56d3b979b589df5e992718cd4b12a27c1c
syymza <syymza@gmail.com>
2015-12-03 10:28:37 +0000

    namespaced users endpoint

--- app/routes/users.server.routes.js
+++ app/routes/users.server.routes.js
@@ -5,11 +5,11 @@ const users = require('../controllers/users.server.controller');
 
 module.exports = (app) => {
 
-    app.route('/users')
+    app.route('/eme-users')
         .get(ensureAuthenticated, users.list)
         .post(ensureAuthenticated, users.create);
 
-    app.route('/users/:userId')
+    app.route('/eme-users/:userId')
         .get(ensureAuthenticated, users.read)
         .patch(ensureAuthenticated, users.patch)
         .delete(ensureAuthenticated, users.delete);
